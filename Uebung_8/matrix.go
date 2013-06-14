98fc36f5edff6893ad8e26c4376dd8added8bb16
Sven Wildermann <git@wildermannonline.de>
2013-06-14 11:55:03 +0200

    nur ein Kommentar geaendert

--- Uebung_8/matrix.go
+++ Uebung_8/matrix.go
@@ -1,31 +1,29 @@
 package main
 import "fmt"
 
-/* Leider habe ich es nicht puenktlich
-geschafft den Code erfolgreich zum compilieren zu bringen*/ 
-
 
 
 func mult(matrix1 [][]int, matrix2 [][]int, e chan bool){
 
     newMa := [][]int{{0,0,0},{0,0,0},{0,0,0}}
     for i := 0 ; i < len(matrix1) ;i++{
-	w :=  make (chan bool)
+	
         for j := 0 ; j < len(matrix2[0]) ;j++ {
+	    w :=  make (chan bool)
             go rowCol(i,j,matrix1,matrix2,newMa,w)
 	    <-w
 	}
 	 
     }
-    fmt.Println(newMa)
-    e<-true
+     fmt.Println(newMa)
+    e <- true
 }
 
 
 func rowCol( r int , c int, matrix1 [][]int, matrix2 [][]int, newMa [][]int,  e chan bool){
     var sum int = 0
-    for i := 0 ; i < len(matrix1) ; i++ {
-        sum += matrix1 [r][i] * matrix1 [i][c]
+    for i := 0 ; i < len(matrix1[1]) ; i++ {
+        sum += matrix1 [r][i] * matrix2 [i][c]
     }
      newMa[r][c] = sum
     e<-true
@@ -40,11 +38,9 @@ func main(){
                          {0,1,0} , 
                          {1,0,0} }
 
- q := make (chan bool)
-
- mult(matrix1 , matrix2, q)
-
- <-q
+  q := make (chan bool)
+  mult(matrix1 , matrix2, q)
+  <-q
 
 
 }
